df9818ad7854fe63e76c07bd253cfe277e623053
fardeen <ittzzfardeen@gmail.com>
2024-07-21 01:05:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+from flask import Flask, request, jsonify, render_template
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('in.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
